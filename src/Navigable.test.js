2cbbb20e78f79ae2b8b61a7aaec6c815bab22829
Fang <windmaomao@gmail.com>
2019-10-26 19:36:51 -0400

    add test for prev and next

--- src/Navigable.test.js
+++ src/Navigable.test.js
@@ -2,6 +2,13 @@ import Navigable from './Navigable'
 
 describe('Navigable', () => {
   let children
+  const items = [1, 2]
+  let activeItem
+  const onSelectItem = item => { activeItem = item }
+  const getArgs = call => {
+    const calls = call.mock.calls
+    return calls.length ? calls[0][0] : {}
+  }
   beforeEach(() => {
     children = jest.fn()
   })  
@@ -14,9 +21,71 @@ describe('Navigable', () => {
     expect(Navigable({ items: [] })).toEqual(null)
   })
 
-  it('should return call children function', () => {
-    Navigable({ items: [], children })
+  it('should call children function', () => {
+    Navigable({ items, children })
+
+    const calls = children.mock.calls    
     expect(children).toHaveBeenCalled()
+    expect(calls.length).toEqual(1)
+  })
+
+  it('should goto a item', () => {
+    activeItem = 1
+    Navigable({ items, activeItem, onSelectItem, children })
+
+    const { goto } = getArgs(children)
+    goto(2)
+    expect(activeItem).toEqual(2)
+  })
+
+  it('should be able to goto the next item', () => {
+    activeItem = 1
+    Navigable({ items, activeItem, onSelectItem, children })
+
+    const { canNext } = getArgs(children)
+    expect(canNext).toEqual(true)
+  })
+
+  it('should goto the next item', () => {
+    activeItem = 1
+    Navigable({ items, activeItem, onSelectItem, children })
+
+    const { next } = getArgs(children)
+    next()
+    expect(activeItem).toEqual(2)
+  })
+
+  it('should not be able to goto the next item', () => {
+    activeItem = 2
+    Navigable({ items, activeItem, onSelectItem, children })
+
+    const { canNext } = getArgs(children)
+    expect(canNext).toEqual(false)
+  })
+
+  it('should be able to goto the prev item', () => {
+    activeItem = 2
+    Navigable({ items, activeItem, onSelectItem, children })
+
+    const { canPrev } = getArgs(children)
+    expect(canPrev).toEqual(true)
+  })
+
+  it('should goto the previous item', () => {
+    activeItem = 2
+    Navigable({ items, activeItem, onSelectItem, children })
+
+    const { prev } = getArgs(children)
+    prev()
+    expect(activeItem).toEqual(1)
+  })
+
+  it('should not be able to goto the prev item', () => {
+    activeItem = 1
+    Navigable({ items, activeItem, onSelectItem, children })
+
+    const { canPrev } = getArgs(children)
+    expect(canPrev).toEqual(false)
   })
 
 })
